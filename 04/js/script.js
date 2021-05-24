9f37ce7b7553212de97103c2793f0d9fdbc0efeb
guillaume-debeire <guillaume@debeire.com>
2021-05-24 14:06:25 +0200

    un peu moins verbeux

---
+++ 04/js/script.js
@@ -0,0 +1,27 @@
+/*
+
+Chacun son tour
+===============
+
+Un intégrateur nous a gentiment préparé des div en `display: none`.
+C'est à nous de les afficher en JS.
+
+Objectifs
+---------
+- Cibler les div avec la class CSS `.box`
+- Afficher les div une à une avec 1 seconde entre chaque
+
+Notions
+-------
+- DOM : document.querySelectorAll ou getElementsByClassName
+- JS : for ou forEach / setTimeout
+
+*/
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box, boxIndex) => {
+    setTimeout(() => {
+        box.style.display = 'block';
+    }, 1000 * boxIndex);
+});
